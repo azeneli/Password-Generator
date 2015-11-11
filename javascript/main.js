82f841b98256fa3dfd5f82bf54d6caeea12b1e84
azeneli <azeneli46@gmail.com>
2015-11-10 20:36:15 -0500

    Password generator function

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,29 @@
 /* CURRENTLY IN: javascript/main.js */
+
+(function(){ // protect da scope
+
+
+	function generatePassword(){
+		var len = 16
+		,pattern = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@+"
+		,newPattern = '';
+
+		for(var i = 0; i <= len; ++i ){
+			var count = pattern.length;
+			var results = newPattern += pattern.charAt( Math.random() * count );	
+		}
+
+		return newPattern;
+	}
+
+	var password = generatePassword();
+	console.log(password);
+
+	// $('.js-button').on('click', function(){
+	
+	// 	generatePassword();
+
+	// });
+	
+
+})();
